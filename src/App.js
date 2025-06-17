757538f92c27298ccaf35acca9506fbe2cbe8074
Lloyd McKie <lloyd.mckie+sourcetree@gmail.com>
2025-06-17 11:12:12 +0800

    feature: add add form

--- src/App.js
+++ src/App.js
@@ -1,9 +1,28 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Radar from "./components/Radar/Radar";
 
 function App() {
 
-    const setup = {
+    //     const [quatrants, addQuadrant] = useState(['tools', 'techniques', 'platforms', 'languages']);
+    //     const [rings, addRings] = useState(['adopt', 'trial', 'assess', 'hold']);
+    // const [data, addData] = useState([
+    //             {
+    //                 name: 'D3',
+    //                 quadrant: 'tools',
+    //                 ring: "assess"
+    //             },
+    //             {
+    //                 name: 'TypeScript',
+    //                 quadrant: 'languages',
+    //                 ring: "trial"
+    //             },
+    //             {
+    //                 name: 'Storybook',
+    //                 quadrant: 'tools',
+    //                 ring: "adopt"
+    //             }
+    //         ]);   
+    const [setup, setSetup] = useState({
         rings: ['adopt', 'trial', 'assess', 'hold'],
         quadrants: ['tools', 'techniques', 'platforms', 'languages'],
         data: [
@@ -23,10 +42,48 @@ function App() {
                 ring: "adopt"
             }
         ]
-    };
+    });
+    const [name,setName]= useState('');
+    const [ring, setRing]= useState('hold');
+    const [quadrant, setQuadrant]= useState('tools')
 
+    const handleSubmit = (event) =>{
+        event.preventDefault();
+        setup.data.push({
+            name, ring, quadrant
+        });
+        setSetup(setup);
+    }
     return (
         <div className="App">
+            <form onSubmit={handleSubmit}>
+                <div>
+                    <label>Name</label>
+                    <input type='text'></input>
+                </div>
+                <div>
+                    <label>Ring</label>
+                    <select >
+                        {
+                            setup.rings.forEach(ring => {
+                                return (<li >{ring}</li>)
+                            })
+                        }
+                    </select>
+                </div>
+                <div>
+
+                    <label>Quadrant</label>
+                    <select >
+                        {
+                            setup.quadrants.forEach(q => {
+                                return (<li >{q}</li>)
+                            })
+                        }
+                    </select>
+                </div>
+                <input type='submit'>Add</input>
+            </form>
             <Radar {...setup} />
         </div>
     );
