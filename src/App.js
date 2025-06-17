926aea9fa7715ebecffd23e69dc30fea4ab9cebf
Lloyd McKie <lloyd.mckie+sourcetree@gmail.com>
2025-06-17 11:12:18 +0800

    feature: add delete form

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React, { useState } from 'react';
 import Radar from "./components/Radar/Radar";
+import { array } from 'prop-types';
 
 function App() {
 
@@ -43,19 +44,26 @@ function App() {
             }
         ]
     });
-    const [name,setName]= useState('');
-    const [ring, setRing]= useState('hold');
-    const [quadrant, setQuadrant]= useState('tools')
+    const [name, setName] = useState('');
+    const [ring, setRing] = useState('hold');
+    const [quadrant, setQuadrant] = useState('tools')
 
-    const handleSubmit = (event) =>{
+    const handleSubmit = (event) => {
         event.preventDefault();
+        // todo handle unique name
         setup.data.push({
             name, ring, quadrant
         });
         setSetup(setup);
     }
+    const handleDelete = (name) => {
+        const index = setup.data.findIndex((v) => { v.name === name });
+        setup.data.splice(index, 1);
+        setSetup(setup);
+    }
     return (
         <div className="App">
+            {/* add form */}
             <form onSubmit={handleSubmit}>
                 <div>
                     <label>Name</label>
@@ -85,6 +93,19 @@ function App() {
                 <input type='submit'>Add</input>
             </form>
             <Radar {...setup} />
+            {/* Edit data */}
+            <div>
+                <label>Delete</label>
+                <select >
+                    {
+                        setup.data.forEach(datum => {
+                            <li onClick={() => {
+                                handleDelete(datum.name);
+                            }}>{datum.name}</li>
+                        })
+                    }
+                </select>
+            </div>
         </div>
     );
 }
